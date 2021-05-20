5c2cd04dc59f15a13281612e1ad6c003089c5f7c
Austin Mckee <captainlantern8@gmail.com>
2021-05-19 22:25:09 -0700

    Reservation form pre data validate

--- front-end/src/ReservationForm.jsx
+++ front-end/src/ReservationForm.jsx
@@ -1,10 +1,20 @@
-import React, { useEffect, useState } from "react";
-
+import React, { useState } from "react";
+import { createReservations } from "./utils/api";
+import { useHistory } from "react-router-dom";
 export default function ReservationForm() {
+	const history = useHistory();
 	const formChange = (event) => {
 		const changeObj = { ...formData };
 		changeObj[event.target.id] = event.target.value;
+		changeObj.people = Number(changeObj.people);
 		setFormData(changeObj);
+	};
+	const theSubmit = (event) => {
+		event.preventDefault();
+		const abortController = new AbortController();
+		createReservations(formData, abortController.signal).then(() =>
+			history.push(`/dashboard?date=${formData.reservation_date}`)
+		);
 	};
 	const [formData, setFormData] = useState({
 		first_name: "John",
@@ -12,9 +22,10 @@ export default function ReservationForm() {
 		mobile_number: null,
 		reservation_date: null,
 		reservation_time: null,
+		people: 1,
 	});
 	return (
-		<form name="reservation">
+		<form name="reservation" onSubmit={theSubmit}>
 			<div className="form-group">
 				<label htmlFor="first_name">First Name</label>
 				<input
@@ -69,7 +80,21 @@ export default function ReservationForm() {
 					pattern="[0-9]{2}:[0-9]{2}"
 					className="form-control"
 				/>
+				<label htmlFor="people">Party Size</label>
+				<input
+					id="people"
+					name="people"
+					onChange={formChange}
+					value={formData.people}
+					type="number"
+					className="form-control"
+					min="1"
+				/>
 			</div>
+
+			<button type="submit" className="btn btn-primary">
+				Submit
+			</button>
 		</form>
 	);
 }
