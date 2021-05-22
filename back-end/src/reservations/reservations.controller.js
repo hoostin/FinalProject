db3beb1284f9e7504312033d08d2f90906e2b746
Austin Mckee <captainlantern8@gmail.com>
2021-05-22 14:25:34 -0700

    cleanup start

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,11 +1,10 @@
 /**
  * List handler for reservation resources
  */
-//const { whereNotExists } = require("../db/connection");
+
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const service = require("./reservations.service");
-const theValidator = require("../../../front-end/src/utils/validateTest");
-//const { isPast, isTuesday } = require("../../../front-end/src/utils/validate");
+const { theValidator } = require("../../../front-end/src/utils/validateTest");
 
 async function list(req, res) {
 	const reservations = await service.listByDate(req.query.date);
@@ -13,92 +12,27 @@ async function list(req, res) {
 		data: [...reservations],
 	});
 }
-function compareKeys(a, b) {
-	const aKeys = Object.keys(a).sort();
-	const bKeys = Object.keys(b).sort();
-	return JSON.stringify(aKeys) === JSON.stringify(bKeys);
-}
 async function validate(req, res, next) {
 	const added = req.body.data;
+	let message;
+	function setError(err) {
+		if (err) message = err.message;
+	}
 	if (!added)
-		next({
+		return next({
 			status: 400,
 			message:
 				"Invalid data format provided. Requires {string: [first_name, last_name, mobile_number], date: reservation_date, time: reservation_time, number: people}",
 		});
-	const test = {
-		first_name: null,
-		last_name: null,
-		mobile_number: null,
-		reservation_date: null,
-		reservation_time: null,
-		people: 0,
-	};
-	//console.log(added);
-	if (compareKeys(added, test)) {
-		for (let theValue in added) {
-			if (theValue === `people`) {
-				console.log(added[theValue]);
-				if (typeof added[theValue] != `number`) {
-					return next({
-						status: 400,
-						message: "people must be a number ",
-					});
-				}
-			}
-			if (theValue === `reservation_date`) {
-				if (!/\d{4}-\d{2}-\d{2}/.test(added[theValue])) {
-					return next({
-						status: 400,
-						message: "reservation_date must be a date ",
-					});
-				}
-				if (theValidator.isPast(added[theValue]))
-					return next({
-						status: 400,
-						message: "reservation_date must be a date in the future ",
-					});
-				if (theValidator.isTuesday(added[theValue]))
-					return next({
-						status: 400,
-						message:
-							"reservation_date must not be a Tuesday we're closed ",
-					});
-			}
-			if (theValue === `reservation_time`) {
-				if (!/[0-9]{2}:[0-9]{2}/.test(added[theValue])) {
-					return next({
-						status: 400,
-						message: "reservation_time must be a time ",
-					});
-				}
-				if (!theValidator.isTimeOpen(added[theValue])) {
-					return next({
-						status: 400,
-						message: "Closed only open 10:30 AM - 10:30 PM with 1hr window",
-					});
-				}
-				if (!theValidator.isTimeValid(added[theValue])) {
-					return next({
-						status: 400,
-						message: " Must be in Future ",
-					});
-				}
-			}
-			if (!added[theValue])
-				return next({
-					status: 400,
-					message:
-						"Invalid data format provided. Requires {string: [first_name, last_name, mobile_number], date: reservation_date, time: reservation_time, number: people}",
-				});
+	if (!theValidator(added, setError) || typeof added.people != "number") {
+		console.log("MESSSSAGE:", message);
+		if (!message) {
+			message = "people must be a number";
 		}
+		return next({ status: 400, message });
+	} else {
 		return next();
 	}
-	next({
-		status: 400,
-		message:
-			"Invalid data format provided. Requires {string: [first_name, last_name, mobile_number], date: reservation_date, time: reservation_time, number: people}",
-	});
 }
 async function create(req, res) {
 	const added = await service.create(req.body.data);
--- front-end/src/utils/utilCodepad.js
+++ front-end/src/utils/utilCodepad.js
@@ -6,22 +6,58 @@
 // no values null
 
 // added = formData
+function compareKeys(a, b) {
+	const aKeys = Object.keys(a).sort();
+	const bKeys = Object.keys(b).sort();
+	return JSON.stringify(aKeys) === JSON.stringify(bKeys);
+}
+function notNull(obj) {
+	for (let key in obj) {
+		if (!obj[key]) return false;
+	}
+	return true;
+}
 
 function theValidator(formData, setError) {
 	setError(null);
+	const test = {
+		first_name: null,
+		last_name: null,
+		mobile_number: null,
+		reservation_date: null,
+		reservation_time: null,
+		people: 0,
+	};
 	let message = "";
-	if (!isTimeOpen(formData.reservation_time)) {
-		message += "  /Closed only open 10:30 AM - 10:30 PM with 1hr window ";
+
+	if (!compareKeys(formData, test) || !notNull(formData)) {
+		message =
+			"Invalid data format provided. Requires {string: [first_name, last_name, mobile_number], date: reservation_date, time: reservation_time, number: people}";
+		setError(message);
+		return false;
 	}
-	if (isTuesday(formData.reservation_date)) {
-		message += "  /Closed Tuesdays";
+
+	if (/\d{4}-\d{2}-\d{2}/.test(formData.reservation_date)) {
+		if (isTuesday(formData.reservation_date)) {
+			message += "  /Closed Tuesdays";
+		}
+		if (isPast(formData.reservation_date)) {
+			message += " /Must be in Future";
+		}
+	} else {
+		message += "reservation_date must be a date";
 	}
-	if (isPast(formData.reservation_date)) {
-		message += " /Must be in Future";
+	if (/[0-9]{2}:[0-9]{2}/.test(formData.reservation_time)) {
+		if (!isTimeOpen(formData.reservation_time)) {
+			message += "  /Closed only open 10:30 AM - 10:30 PM with 1hr window ";
+		}
+		if (!isTimeValid(formData.reservation_time, formData.reservation_date)) {
+			message += "/ Must be in Future ";
+		}
+	} else {
+		message += "reservation_time must be a time ";
 	}
-	if (!isTimeValid(formData.reservation_time, formData.reservation_date)) {
-		message += "/ Must be in Future ";
-	}
+
 	if (message.length) {
 		setError(new Error(message));
 		return false;
